26357be75c006f5b0aa4b79b5e7015311b1b3f72
SRINIVAS <KALYAN@KALYAN-PC>
2015-10-22 02:20:32 -0400

    Partial Setup of Network

---
+++ src/main/scala/Range.scala
@@ -0,0 +1,35 @@
+import java.math.BigInteger
+
+/**
+ * @author KALYAN
+ */
+
+class Range(leftInclude:Boolean, leftValue:BigInt, rightValue:BigInt, rightInclude:Boolean){
+  
+  def isInclude(value:BigInt): Boolean = {
+
+    if(leftValue == rightValue){   
+      if(leftInclude == false && rightInclude == false && value == leftValue)
+        return false
+      else
+        return true
+    }
+    else if(leftValue < rightValue){
+      if((value == leftValue && leftInclude == true) || (value == rightValue && rightInclude == true) || (value > leftValue && value < rightValue))
+        return true
+      else
+        return false
+    }
+    else{
+      if((value == leftValue && leftInclude == true) || (value == rightValue && rightInclude == true) || (value > leftValue || value < rightValue))
+        return true
+      else
+        return false
+    }
+  }
+
+  def getEnd(): BigInt ={
+    return rightValue
+  }
+
+}
--- src/main/scala/project3.scala
+++ src/main/scala/project3.scala
@@ -4,13 +4,17 @@ import akka.actor.{ ActorRef, ActorSystem, Props, Actor }
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
+import scala.math.BigInt
 
 import java.security.MessageDigest
 
 //Messages
 sealed trait ChordMessage
 
-case class InitializeFingerTable() extends ChordMessage
+case class Initialize(nodeName:String, nodeHash:BigInt, isFirstNode:Boolean) extends ChordMessage
+case class findFinger(node:ActorRef, i:Int, start:BigInt) extends ChordMessage
+case class updateFinger(before:BigInt, i:Int, node:ActorRef, nodeHash:BigInt) extends ChordMessage
+case class foundFinger(i:Int, successor:ActorRef) extends ChordMessage
 
 object project3 extends App{
   
@@ -20,6 +24,9 @@ object project3 extends App{
     */
     var numNodes = 0
     var numRequests = 0
+    
+    var ID_LENGTH = 160
+    var HASH_TYPE = "SHA-1"
     
     if(args.length != 2){
       println(" !!! Invalid Input !!!\n "+
@@ -34,117 +41,150 @@ object project3 extends App{
       chord_protocol(numNodes, numRequests)
     }
   
-    def chord_protocol(numNodes : Int, numRequests : Int){
-      
-      val system = ActorSystem("project3")
-      
-      val master = system.actorOf(Props(new Master(numNodes, numRequests, system)));
-      
-      master ! "start"
-    }
-}  
-
-class Master (numNodes : Int, numRequests : Int, system :ActorSystem) extends Actor{
-
-    /*
-     *  
-     */
-    
     var chordNodes = new ArrayBuffer[ActorRef]()
         
-    println("Started building network")
-    
-    for (i <- 0 until numNodes-1) {
-        chordNodes += system.actorOf(Props(new ChordNode(numNodes, numRequests, i)), name = "Node" + i)
-    }
-    
-    println("Completed building network")
-    
-    var keyList = Array(0,3,4,7)
-    var keyHashList = Array[String]()
-    var nodeMatchList = Array[BigInt]()
-    
-    for (j <- 0 until keyList.length-1) {
-        
-      var keyHash : String = getSHA1(keyList(j).toString())
-      keyHashList = keyHashList :+ keyHash
-              
-      var node : BigInt = BigInt(keyHashList(j).trim(), 16);
-      nodeMatchList = nodeMatchList :+ node
+    def chord_protocol(numNodes : Int, numRequests : Int){   
+      val system = ActorSystem("project3")
       
-    }
-    
-    def getKeyIdSuccessor(input : BigInt) : BigInt =
-    {
-       for (k <- 0 until chordNodes.size-1)
-       {
-         
-       }
-    } 
-    
-    def getSHA1(input:String): String =
-    {
-        val md = MessageDigest.getInstance("SHA-1")
-        val hexString= md.digest(input.getBytes("UTF-8")).map("%02x".format(_)).mkString
-        return hexString
-    }
-    
-    def receive = {
-      case "start" =>
-        
-    }
-  }
-
-class ChordNode (numNodes : Int, numRequests : Int, identifier : Int) extends Actor{
-  
-    var predecessor : String = null
-    var successors = new ArrayBuffer[String]
-    
-
-    
-    var nodeHash : String = getHash(self.path.name)
-    //var keyHash : String = getHash(key)
-    
-    var DEFAULT_METHOD =  "SHA-1"
-  
-    def receive = {
-    
-      case "abc" => {
-        
-        }
-    }
-    
-    def InitializeFinger() {  
+      createNetwork("node1", system)
       
-      System.out.println("Started Finger Initialization");
-      
-      if (successors.isEmpty)
-      {
-        //successors += keyHash
+      for (i <- 1 until numNodes-1) {
+        joinNetwork("node"+i, system)
       }
     }
-  
-    def findPredecessor {
-    
+      
+    def createNetwork(nodeName : String, sys : ActorSystem) {
+      
+      System.out.println(node + " is the first node in network")
+      
+      val nodeHash = getHash(nodeName)
+      val node = sys.actorOf(Props(new ChordNode(numRequests, nodeName, nodeHash, "create")));
+      var isFirstNode : Boolean = true
+      
+      node ! Initialize(nodeName, nodeHash, isFirstNode)
+      Thread.sleep(1000)
+       
     }
-  
-    def findNodeSuccessor {
     
+    def joinNetwork(node : String, sys : ActorSystem) {
+      
+      System.out.println(node + " wants to join the network")
+      val nodeHash = getHash(node)
+      chordNodes += sys.actorOf(Props(new ChordNode(numRequests, node, nodeHash, "join")));
     }
     
-    def findIdSuccessor {
+    def leaveNetwork(node : String) {
+    
+      System.out.println(node + " wants to leave the network")
       
     }
+    
+    def getHash(input:String): BigInt = {
+        val md = MessageDigest.getInstance(HASH_TYPE)
+        val hashString = BigInt(md.digest(input.getBytes("UTF-8")).map("%02x".format(_)).mkString.trim(),16)
+        return hashString
+    }
+    
+    class ChordNode (numRequests : Int, node : String, nodeHash : BigInt, opType : String) extends Actor{
   
-    def updateFinger {
+        var isExists : Boolean = false
+        var predecessor = self
+        var successor = self 
+        var fingerTable = new Array[Finger](ID_LENGTH)
+        
+        for(i <- 0 until ID_LENGTH-1) {
+          val start = (nodeHash + BigInt(2).pow(i-1)) % (BigInt(2).pow(ID_LENGTH))
+          val end = (nodeHash + BigInt(2).pow(i)) % (BigInt(2).pow(ID_LENGTH))
+          val range= new Range(true, start, end, false)
+        
+          fingerTable(i)= new Finger(start, range, self)
+        }
+  
+        def InitializeFinger() {  
+            System.out.println("Started Finger Initialization");
+            
+            fingerTable(0).setNode(successor)
+      
+            for(i<-0 until ID_LENGTH-1){
+                val range = new Range(true, nodeHash, fingerTable(i).getHash(), true)
+      
+                if(range.isInclude(fingerTable(i+1).getStart())) {
+                    fingerTable(i).setNode(fingerTable(i-1).getNode())
+                }else{
+                  if(exist!=null)
+                    exist ! findFinger(self, i, fingerTable(i).getStart())
+                }
+            }
+        }
+  
+        def updateOthers():Unit = {
+          for(i <- 0 to ID_LENGTH-1) {
+            val position=(nodeHash - BigInt(2).pow(i)+BigInt(2).pow(ID_LENGTH)+1)%BigInt(2).pow(ID_LENGTH)
+            successor ! updateFinger(position, i, self, nodeHash)
+          }
+        }
     
-    } 
+        def getHash(input:String): String = {
+            val md = MessageDigest.getInstance(HASH_TYPE)
+            val hexString= md.digest(input.getBytes("UTF-8")).map("%02x".format(_)).mkString
+            return hexString
+        } 
     
-    def getHash(input:String): String =
-    {
-        val md = MessageDigest.getInstance(DEFAULT_METHOD)
-        val hexString= md.digest(input.getBytes("UTF-8")).map("%02x".format(_)).mkString
-        return hexString
-    }    
- 
-}
+        def closestPreceedingFinger(id:BigInt): ActorRef = {   
+            val range=new Range(false, nodeHash, id, false)
+          
+            for(i <- ID_LENGTH-1 to 0 by -1){
+              if(range.isInclude(fingerTable(i).getHash()))
+                return fingerTable(i).node;
+            }
+            return self;
+          }
+    
+        override def receive: Receive = {
+    
+          case Initialize(nodeName:String, nodeHash:BigInt, isFirstNode : Boolean)=>{
+              
+              
+          }
+            
+          case findFinger(node:ActorRef, i:Int, start:BigInt)=>{
+                
+                val range = new Range(false, nodeHash, fingerTable(0).getHash(), true)
+              
+                if(range.isInclude(start)){
+                  node ! foundFinger(i,successor)
+                }else{
+                  val target = closestPreceedingFinger(start)
+                  target ! findFinger(node,i,start)
+                }
+            }
+        
+            case foundFinger(i:Int, successor:ActorRef)=>{
+                this.fingerTable(i).setNode(successor)
+            }
+        
+            case updateFinger(before:BigInt,i:Int,node:ActorRef,nodeHash:BigInt) => { 
+        
+                if(node != self) {
+              
+                  val range1 = new Range(false, nodeHash, fingerTable(0).getHash(), true)
+                  
+                  if (range1.isInclude(before)) { 
+                
+                      val range2=new Range(false, nodeHash, fingerTable(i).getHash(), false)
+                                
+                      if(range2.isInclude(nodeHash)){
+                  fingerTable(i).setNode(node)
+                  predecessor ! updateFinger(nodeHash, i, node, nodeHash)
+                }
+              }else{
+                val target = closestPreceedingFinger(before)
+                target ! updateFinger(before,i,node,nodeHash)
+              }
+            }
+          }
+      }
+  }
+}  
+
+
